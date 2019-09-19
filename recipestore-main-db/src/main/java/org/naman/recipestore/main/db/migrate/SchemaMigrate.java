96143c49da5a3fc984615b9c0691a4aa8c251890
namangup <naman.a.gupta@capgemini.com>
2019-09-20 01:47:28 +0530

    added db dependency.

---
+++ recipestore-main-db/src/main/java/org/naman/recipestore/main/db/migrate/SchemaMigrate.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 Capgemini. All rights reserved.
+ */
+package org.naman.recipestore.main.db.migrate;
+
+/**
+ * TODO namangup: Short description of the class ...
+ *
+ * @author namangup
+ */
+public class SchemaMigrate
+{
+
+}
